28584d78210dc165738d331295fbb31078e09a22
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-08-29 02:29:44 -0500

    established routes, ran into challenge with deprecated methods

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express');
-const { Mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const mongoose = require('mongoose');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-	// useFindAndModify: false,
+	useFindAndModify: false,
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
 
-//Use this to log mongo queries being executed
+// Use this to log mongo queries being executed!
 mongoose.set('debug', true);
+
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
